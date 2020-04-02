aa9da91ea0e7c6eebc4bd09eabe95ac1f827de80
Samuel de Morais <samucatnb@gmail.com>
2020-04-02 17:26:06 -0300

    implementado categoria e produtos

--- CursoMVC/Models/Produto.cs
+++ CursoMVC/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,22 @@ namespace CursoMVC.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Campo Descrição é obrigatório")]
         public string Descricao { get; set; }
+        [Range(1,9999, ErrorMessage = "Valor fora da faixa permitida")]
         public decimal Quantidade { get; set; }
 
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
 
+        /// <summary>
+        /// criei propriedade para devolver codigo e nome da categoria para mostrar em tela
+        /// </summary>
+        public string Categoria_CodigoNome
+        {
+            get { return CategoriaId.ToString() + " - " + string.IsNullOrEmpty(Categoria.Descricao)?? ""; }
 
+        }
     }
 }
